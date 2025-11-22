3377263886a5e901f1a4c94ec4f68aada59caef0
Tristan Sizik <tristansizikcode@gmail.com>
2025-11-21 20:17:48 -0800

    Consolidated code from Application Home to Calculator, following SOLID principles. I want ApplicationHome to only have *running calculator* in there. More modular

--- src/projectfolder/ApplicationHome.java
+++ src/projectfolder/ApplicationHome.java
@@ -13,64 +13,7 @@ public class ApplicationHome {
 	private static boolean debug = true;
 	
 	public static void main(String[] args) throws IOException {
-		System.out.println("Hi There, I'm a Calculator! ");
-		System.out.println("Insert Really Cool Design here ~~");
-		Calculator calc = new Calculator();
-		
-		int inputValue;
-		StringBuilder sb = new StringBuilder();
-		while ((inputValue = System.in.read()) != -1 ) {
-			if(inputValue == '\n') {
-				break;
-			}
-			sb.append((char) inputValue);
-		}
-		
-		String readValue = sb.toString().trim().replace(" " , "");
-		
-		if(debug) {
-			System.out.println(readValue);
-		}
-			
-		double value = Double.NaN;
-		if( readValue.contains("*")) {
-			int pos = readValue.indexOf('*');
-//			System.out.println(pos);
-			
-			double valA = Double.valueOf(readValue.substring(0, pos));
-			double valB = Double.valueOf(readValue.substring(pos+1, readValue.length()));
-//			System.out.println("values " + val1 + " " +  val2);
-			
-//			String[] vals = readValue.split("*"); //wasn't working TODO: debug
-			
-//			String vals = readValue.split("*")[0];
-//			String vals2 = readValue.split("*")[1];
-//			double valA = Double.valueOf(vals[0]);
-//			double valB = Double.valueOf(vals[1]);
-			value = calc.multiplyOp(valA, valB);
-		}
-		if( readValue.contains("+")) {
-			int pos = readValue.indexOf('+');
-			double valA = Double.valueOf(readValue.substring(0, pos));
-			double valB = Double.valueOf(readValue.substring(pos+1, readValue.length()));
-			
-			value = calc.addOp(valA, valB);
-		}
-		if( readValue.contains("-")) {
-			int pos = readValue.indexOf('-');
-			double valA = Double.valueOf(readValue.substring(0, pos));
-			double valB = Double.valueOf(readValue.substring(pos+1, readValue.length()));
-			
-			value = calc.subtractOp(valA, valB);
-		}
-		if( readValue.contains("/")) {
-			int pos = readValue.indexOf('/');
-			double valA = Double.valueOf(readValue.substring(0, pos));
-			double valB = Double.valueOf(readValue.substring(pos+1, readValue.length()));
-			value = calc.divideOp(valA, valB);
-		}
-		
-		System.out.println(value);
+		Calculator calc = new Calculator(debug);
+		calc.runCalculator();
 	}
-
 }
--- src/projectfolder/Calculator.java
+++ src/projectfolder/Calculator.java
@@ -1,13 +1,139 @@
 package projectfolder;
 
+import java.io.IOException;
+
 //should probably be a singleton. only one will exist at a time
 public class Calculator {
 
-	double existingValue = 0.0;
+	
+	boolean debug = false;
 
 	
-	public Calculator() {
-		System.out.println("Sup, I'm Calc");
+	public Calculator(boolean _debug) {
+		this.debug = _debug;
+	}
+	
+	private void coolCosmetic() {
+		
+		System.out.println("/////CCCCCCCC/////////CCCCCCCC/////////CCCCCCCC/////CCC/////////");
+		System.out.println("////CCCCCCCCCC///////CCCCCCCCCC///////CCCCCCCCCC////CCC/////////");
+		System.out.println("///CCC//////CCC/////CCC//////CCC/////CCC//////CCC///CCC/////////");
+		System.out.println("///CCC//////////////CCC//////CCC/////CCC//////CCC///CCC/////////");
+		System.out.println("///CCC//////CCC/////CCC//////CCC/////CCC//////CCC///CCC/////////");
+		System.out.println("////CCCCCCCCCC///////CCCCCCCCCC///////CCCCCCCCCC////CCCCCCCCC///");
+		System.out.println("/////CCCCCCCC/////////CCCCCCCC/////////CCCCCCCC/////CCCCCCCCC///");
+		System.out.println("////////////////////////////////////////////////////////////////");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		
+		
+		System.out.println("/////CCCCCCCC////////////CCC//////////CCC//////////CCCCCCCC/////");
+		System.out.println("////CCCCCCCCCC//////////CCCCC/////////CCC/////////CCCCCCCCCC////");
+		System.out.println("///CCC//////CCC////////CCC/CCC////////CCC////////CCC//////CCC///");
+		System.out.println("///CCC////////////////CCC///CCC///////CCC////////CCC////////////");
+		System.out.println("///CCC//////CCC//////CCCCCCCCCCC//////CCC////////CCC//////CCC///");
+		System.out.println("////CCCCCCCCCC//////CCC///////CCCC////CCCCCCCCC///CCCCCCCCCC////");
+		System.out.println("/////CCCCCCCC//////CCC/////////CCC////CCCCCCCCC////CCCCCCCC/////");
+		System.out.println("////////////////////////////////////////////////////////////////");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		
+		
+		
+	}
+	
+	
+	
+	//Requires two operators, doesn't have existing value carry over
+	public void runCalculatorReference() throws IOException {
+		coolCosmetic();
+		
+		
+		double existingValue = Double.NaN;
+		String userInput = "";
+		int charReader = 0;
+		double calculationValue = 0.0;
+		
+		StringBuilder sb = new StringBuilder();
+		while ((charReader = System.in.read()) != -1 ) {
+			if(charReader == '\n') {
+				userInput = sb.toString().trim().replace(" " , "");
+				if(debug) {
+					System.out.println(userInput);
+				}
+				
+				calculationValue = decideOperator(userInput);
+				System.out.println(calculationValue);
+				sb.setLength(0);
+			}
+			else if(charReader == '\t') {
+				break;
+			}
+			sb.append((char) charReader);
+		}
+		System.out.println("Closing calculator :)");
+	}
+	
+	//Requires two operators, doesn't have existing value carry over
+	public void runCalculator() throws IOException {
+		coolCosmetic();
+		
+		double existingValue = Double.NaN;
+		String userInput = "";
+		int charReader = 0;
+		double calculationValue = 0.0;
+		
+		StringBuilder sb = new StringBuilder();
+		while ((charReader = System.in.read()) != -1 ) {
+			if(charReader == '\n') {
+				userInput = sb.toString().trim().replace(" " , "");
+				if(debug) {
+					System.out.println(userInput);
+				}
+				
+				calculationValue = decideOperator(userInput);
+				System.out.println(calculationValue);
+				sb.setLength(0);
+			}
+			else if(charReader == '\t') {
+				break;
+			}
+			sb.append((char) charReader);
+		}
+		System.out.println("Closing calculator :)");
+	}
+	
+	private double decideOperator(String _strFormula) {
+		double returnValue = Double.NaN;
+		
+		if( _strFormula.contains("*")) {
+			int pos = _strFormula.indexOf('*');
+			
+			double valA = Double.valueOf(_strFormula.substring(0, pos));
+			double valB = Double.valueOf(_strFormula.substring(pos+1, _strFormula.length()));
+			returnValue = multiplyOp(valA, valB);
+		}
+		if( _strFormula.contains("+")) {
+			int pos = _strFormula.indexOf('+');
+			
+			double valA = Double.valueOf(_strFormula.substring(0, pos));
+			double valB = Double.valueOf(_strFormula.substring(pos+1, _strFormula.length()));
+			returnValue = addOp(valA, valB);
+		}
+		if( _strFormula.contains("-")) {
+			int pos = _strFormula.indexOf('-');
+			
+			double valA = Double.valueOf(_strFormula.substring(0, pos));
+			double valB = Double.valueOf(_strFormula.substring(pos+1, _strFormula.length()));
+			returnValue = subtractOp(valA, valB);
+		}
+		if( _strFormula.contains("/")) {
+			int pos = _strFormula.indexOf('/');
+			
+			double valA = Double.valueOf(_strFormula.substring(0, pos));
+			double valB = Double.valueOf(_strFormula.substring(pos+1, _strFormula.length()));
+			returnValue = divideOp(valA, valB);
+		}
+		
+		return returnValue;
 	}
 	
 	public double addOp(double _inputValue1, double _inputValue2) {
